942fe4f877192e6827be636ff58a6c108401180a
francisco-leiva <leivafrancisco631@gmail.com>
2023-04-26 18:34:46 -0300

    Add new icon and change styles

---
+++ src/components/Icons.jsx
@@ -0,0 +1,22 @@
+export function CodeIcon() {
+  return (
+    <svg
+      className='w-12 h-12 text-white'
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      fill='none'
+      strokeWidth='1.5'
+      color='#000'
+      viewBox='0 0 24 24'
+    >
+      <path
+        className='stroke-white'
+        stroke='#000'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M13.5 6L10 18.5m-3.5-10L3 12l3.5 3.5m11-7L21 12l-3.5 3.5'
+      ></path>
+    </svg>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { CodeIcon } from './Icons';
 
 export default function Navbar() {
   const links = [
@@ -9,16 +10,24 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className='w-full h-14  bg-black'>
-      <ul className='flex justify-center items-center gap-4'>
-        {links.map((link, index) => {
-          return (
-            <li key={index} className='text-white'>
-              <Link href={link.href}>{link.name}</Link>
-            </li>
-          );
-        })}
-      </ul>
+    <nav className='w-full h-16 bg-black sticky top-0 text-lg'>
+      <div className='max-w-4xl h-full mx-auto flex justify-between items-center'>
+        <div>
+          <Link href='/'>
+            <CodeIcon />
+          </Link>
+        </div>
+
+        <ul className='text-white'>
+          {links.map((link, index) => {
+            return (
+              <li key={index} className='inline mx-5 first:ml-0 last:mr-0'>
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 }
