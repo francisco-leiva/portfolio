f2c48570a9bdb9e50caf65d55ac9d9faf08ef63b
francisco-leiva <leivafrancisco631@gmail.com>
2023-04-28 17:48:10 -0300

    Add contact form structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,40 @@
 export default function Contact() {
   return (
     <section id='contact' className='max-w-4xl h-80 mx-auto my-10'>
-      Contacto
+      <h3>Contacto</h3>
+
+      <form>
+        <div>
+          <label htmlFor='form_name'>Nombre</label>
+          <input
+            id='form_name'
+            type='text'
+            className='border border-black border-solid'
+          />
+        </div>
+
+        <div>
+          <label htmlFor='form_mail'>Email</label>
+          <input
+            id='form_mail'
+            type='email'
+            className='border border-black border-solid'
+          />
+        </div>
+
+        <div>
+          <label htmlFor='form_message'>Mensaje</label>
+          <textarea
+            id='form_message'
+            type='text'
+            className='border border-black border-solid'
+          />
+        </div>
+
+        <button className='bg-red-600 border border-black border-solid'>
+          Enviar
+        </button>
+      </form>
     </section>
   );
 }
